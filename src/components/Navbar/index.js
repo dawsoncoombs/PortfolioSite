3de6b0bfbddfa98385a5bec87d3d5fc2c6ecd04e
Dawson Coombs <dawsontech0@gmail.com>
2023-01-24 19:03:56 -0500

    Implemented NavBar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import {FaBars} from 'react-icons/fa'
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink
+} from './NavbarElements'
+
+const Navbar = () => {
+  return (
+    <div>
+        <Nav>
+          <NavbarContainer>
+            <NavLogo to="/">dawson.Dev</NavLogo>
+            <MobileIcon>
+              <FaBars />
+            </MobileIcon>
+            <NavMenu>
+              <NavItem>
+                <NavLinks to='about'>About</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to='myWork'>My Work</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to='skills'>Skills</NavLinks>
+              </NavItem>
+            </NavMenu>
+            <NavBtn>
+              <NavBtnLink to='contactMe'>Contact Me</NavBtnLink>
+            </NavBtn>
+          </NavbarContainer>
+        </Nav>
+    </div>
+  )
+}
+
+export default Navbar
